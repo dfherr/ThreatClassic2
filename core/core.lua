969df4429f45262757fd0fbd5404140ebbccddfb
Dennis-Florian Herr <deflo.herr@gmail.com>
2020-04-12 14:47:22 +0200

    enable threat updates out of combat #25

--- core/core.lua
+++ core/core.lua
@@ -730,14 +730,12 @@ end
 function TC2:PLAYER_REGEN_DISABLED(...)
 	UpdatePlayerTarget() -- for friendly mobs that turn hostile like vaelastrasz
 	C.frame.test = false
-	ThreatLib.RegisterCallback(self, "ThreatUpdated", ThreatUpdated)
 	CheckStatus()
 end
 
 function TC2:PLAYER_REGEN_ENABLED(...)
 	-- collectgarbage()
 	C.frame.test = false
-	ThreatLib.UnregisterCallback(self, "ThreatUpdated", ThreatUpdated)
 	CheckStatus()
 end
 
